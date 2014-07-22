5ba6b32201ab73b74fec91d689f2a79314469f07
Badri Sugavanam <bnarayanan@gmail.com>
2014-07-22 23:02:13 +1000

    Initial Stub of Pom project for Webdriver C# work

---
+++ UnitTestProject3/Login.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support;
+
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace CC
+{
+    public class Login
+    {
+        private OpenQA.Selenium.IWebDriver _driver;
+
+        public Login(OpenQA.Selenium.IWebDriver driver)
+        {
+            this._driver = driver;
+            OpenQA.Selenium.Support.PageObjects.PageFactory.InitElements(driver, this);
+        }
+
+        
+
+       
+
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = OpenQA.Selenium.Support.PageObjects.How.Id, Using = "txtAccountNo")]
+        private OpenQA.Selenium.IWebElement tb_accountID;
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = OpenQA.Selenium.Support.PageObjects.How.Id, Using = "txtUserName")]
+        private OpenQA.Selenium.IWebElement tb_userID;
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = OpenQA.Selenium.Support.PageObjects.How.Id, Using = "txtLoginPassword")]
+        private OpenQA.Selenium.IWebElement tb_password;
+        [OpenQA.Selenium.Support.PageObjects.FindsBy(How = OpenQA.Selenium.Support.PageObjects.How.Id, Using = "ibtnLogin")]
+        private OpenQA.Selenium.IWebElement btn_login;
+        private OpenQA.Selenium.IWebDriver ff;
+
+        public OpenQA.Selenium.IWebElement get_accountID()
+        {
+            return this.tb_accountID;
+        }
+
+        public void set_accountID(OpenQA.Selenium.IWebElement ele)
+        {
+            this.tb_accountID = ele;
+        }
+
+        public OpenQA.Selenium.IWebElement get_userID()
+        {
+            return this.tb_userID;
+        }
+
+        public void set_userID(OpenQA.Selenium.IWebElement ele)
+        {
+            this.tb_userID = ele;
+        }
+
+        public OpenQA.Selenium.IWebElement get_password()
+        {
+            return this.tb_password;
+        }
+
+        public void set_password(OpenQA.Selenium.IWebElement ele)
+        {
+            this.tb_password = ele;
+        }
+
+        public OpenQA.Selenium.IWebElement get_login()
+        {
+            return this.btn_login;
+        }
+
+        public void set_login(OpenQA.Selenium.IWebElement ele)
+        {
+            this.btn_login = ele;
+        }
+
+        public void loginPage(String username, String password)
+        {
+            get_userID().SendKeys(username);
+            get_password().SendKeys(password);
+            get_login().Click();
+        }
+
+
+    }
+}
---
+++ UnitTestProject3/UnitTest1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using CC;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace CCWebDriver
+{
+    [TestFixture]
+    public class UnitTest1
+    {   
+        [Test]
+        [TestCase("badri", "password", "https://www.cc/notifications/CCLogin.aspx")]
+        public void LoginTest(String u, String p, String url)
+        {
+           // Console.Out.Write("hello");
+            //WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+            DesiredCapabilities dc = new DesiredCapabilities();
+           // System.Environment.SetEnvironmentVariable("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+            ChromeOptions options = new ChromeOptions();
+            options.BinaryLocation = "C:\\chromedriver_win32\\chromedriver.exe";
+            
+dc.SetCapability(ChromeOptions.Capability, options);
+            
+            
+            //dc.SetCapability("webdriver.chrome.driver", "C:\\chromedriver_win32\\chromedriver.exe");
+OpenQA.Selenium.IWebDriver ff = new FirefoxDriver();
+                // new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"), dc);
+  //          OpenQA.Selenium.IWebDriver ff = new RemoteWebDriver(new Uri("http://localhost:4444/wd/hub"),DesiredCapabilities.Firefox(),TimeSpan.FromMinutes(2));
+            //WebDriverWait wait = new WebDriverWait(ff, TimeSpan.FromSeconds(10));
+            
+            ff.Navigate().GoToUrl(url);     
+
+            Login l = new Login(ff);
+            l.loginPage(u, p);
+        }
+
+        [TestCase()]
+        public void Loginn()
+        {
+            
+        }
+    }
+}
